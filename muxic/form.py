30dc42dfbe7c62c1054b9a2cd96ace255ca6832c
SPlayer <splayer1295@gmail.com>
2018-03-07 23:03:51 +0700

    Add User Registration - Need fix layout

--- muxic/form.py
+++ muxic/form.py
@@ -3,8 +3,19 @@ from django import forms
 
 
 class UserForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput)
+    email = forms.CharField(widget=forms.EmailInput)
     password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+    def clean(self):
+        clean_data = super(UserForm, self).clean()
+        password = clean_data.get("password")
+        confirm_password = clean_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Mật khẩu bạn nhập không trùng!")
--- muxic/views.py
+++ muxic/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
 from django.views.generic.base import View
 
 # Create your views here.
@@ -21,19 +22,44 @@ class UserView(TemplateView):
 
 class UserFormView(View):
     form_class = UserForm
+    # success_url = 'muxic:index'
     template_name = 'muxic/registration_form.html'
+
+    # def form_valid(self, form):
+    #     # form = self.form_class(request.POST)
+    #     user = form.save(commit=False)
+    #
+    #     # Clean data
+    #     username = form.cleaned_data['username']
+    #     email = form.cleaned_data['email']
+    #     password = form.cleaned_data['password']
+    #     user.set_password(password)
+    #     user.save()
+    #
+    #     # returns User objects if credentials are correct
+    #     user = authenticate(username=username, password=password)
+    #
+    #     # login(request, user)
+    #     # return redirect('music:index')
+    #
+    #     if user is not None:
+    #         if user.is_active:
+    #             login(request, user)
+    #             return redirect('music:index')
+    #
+    #     return render(request, self.template_name, {'form': form})
 
     # Display blank form
     def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form': form})
+        if request.method != 'POST':
+            form = self.form_class(None)
+            return render(request, self.template_name, {'form': form})
 
     # Process form data
     def post(self, request):
         form = self.form_class(request.POST)
 
         if form.is_valid():
-
             user = form.save(commit=False)
 
             # Clean data
@@ -45,9 +71,12 @@ class UserFormView(View):
             # returns User objects if credentials are correct
             user = authenticate(username=username, password=password)
 
+            # login(request, user)
+            # return redirect('muxic:index')
+
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return redirect('music:index')
+                    return redirect('muxic:index')
 
         return render(request, self.template_name, {'form': form})
